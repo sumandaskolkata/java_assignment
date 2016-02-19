7ba1c83956a0fff49b8ce998a289bf0d61147bf1
sumandaskolkata <sumandas.kolkata@yahoo.com>
2016-02-19 12:21:12 +0530

    Matrix multiplication added

--- Matrix.java
+++ Matrix.java
@@ -22,29 +22,58 @@ class Matrix {
       }
     }
 
-    public Matrix addMatrix(Matrix another){
+    public Matrix addMatrix(Matrix otherMatrix){
+      if(isNotSameOrder(otherMatrix)){
+        return new Matrix(0,0);
+      }
       Matrix resultMatrix = new Matrix(this.rows,this.columns);
       for (int i=0;i<rows;i++){
         for (int j=0;j<columns ;j++) {
-          int additionResult =  this.matrix[i][j]+ another.getElementAt(i,j);
-          resultMatrix.insertAt(i,j,additionResult);
+          int additionResult =  this.matrix[i][j]+ otherMatrix.matrix[i][j];
+          resultMatrix.insertValueAt(i,j,additionResult);
+        }
+      }
+      return resultMatrix;
+    }
+    private boolean isNotSameOrder(Matrix otherMatrix){
+      return !((this.rows == otherMatrix.rows) && (this.columns == otherMatrix.columns));
+    }
+
+
+    private boolean isMultiplicable(Matrix otherMatrix){
+      return this.rows == otherMatrix.columns;
+    }
+
+
+    public Matrix multiply(Matrix otherMatrix){
+      Matrix resultMatrix = new Matrix(this.rows,otherMatrix.columns);
+      int resultOfOneRowAndColumn = 0;
+      for (int i=0;i<this.rows ;i++) {
+        for (int k=0;k<otherMatrix.columns;k++) {
+          for (int j=0;j<this.columns ;j++) {
+            int multiplyResult = this.matrix[k][j]*otherMatrix.matrix[j][k];
+            // System.out.print(multiplyResult+"--->");
+            resultOfOneRowAndColumn+=multiplyResult;
+          }
+        // System.out.print(resultOfOneRowAndColumn+"<<<<-------");
+        resultMatrix.matrix[i][k] = resultOfOneRowAndColumn;
+        resultOfOneRowAndColumn=0;
         }
       }
       return resultMatrix;
     }
 
-    public int[][] getMatrix(){
-      return matrix;
+    public boolean isSameAs(Matrix otherMatrix){
+      return Arrays.deepEquals(this.matrix,otherMatrix.matrix);
+    }
+    public boolean isValueAt(int row,int column,int value){
+      return matrix[row][column] == value;
     }
 
-    public void insertAt(int row,int columns,int element){
+    public void insertValueAt(int row,int columns,int element){
       matrix[row][columns] = element;
     }
 
-    public int getElementAt (int row,int columns){
-      return matrix[row][columns];
-    }
- 
     public void print(){
       for (int i=0;i<this.rows ; i++) {
         for (int j=0;j<this.columns ;j++) {
--- MatrixTest.java
+++ MatrixTest.java
@@ -6,15 +6,24 @@ import org.junit.Test;
 
 public class MatrixTest {
   @Test
-  public void createArray(){
+  public void createMatrixAndAssertTwoMatrix(){
     Matrix a = new Matrix(2,4);
     a.fillMatrix(5);
     Matrix b = new Matrix(2,4);
     b.fillMatrix(5);
-    org.junit.Assert.assertArrayEquals(a.getMatrix(),b.getMatrix());
+    org.junit.Assert.assertTrue(a.isSameAs(b));
   }
   @Test
-  public void addMatrix(){
+  public void insertValueAtSpecificPosition(){
+    Matrix a = new Matrix(2,2);
+    a.insertValueAt(0,0,3);
+    a.insertValueAt(1,1,23);
+
+    org.junit.Assert.assertTrue(a.isValueAt(0,0,3));
+    org.junit.Assert.assertTrue(a.isValueAt(1,1,23));
+  }
+  @Test
+  public void addTwoMatrixAndAssertResult(){
   	Matrix a = new Matrix(2,4);
     a.fillMatrix(5);
     Matrix b = new Matrix(2,4);
@@ -22,6 +31,37 @@ public class MatrixTest {
     Matrix resultMatrix = a.addMatrix(b);
     Matrix expected = new Matrix(2,4);
     expected.fillMatrix(10);
-    org.junit.Assert.assertArrayEquals(expected.getMatrix(),resultMatrix.getMatrix());
+    org.junit.Assert.assertTrue(expected.isSameAs(resultMatrix));
+  }
+  @Test
+  public void returnEmptyMatrixThenAdditionIsNotPossible(){
+  	Matrix a = new Matrix(2,2);
+    a.fillMatrix(5);
+    Matrix b = new Matrix(2,4);
+    b.fillMatrix(5);
+    Matrix resultMatrix = a.addMatrix(b);
+    Matrix expected = new Matrix(0,0);
+    org.junit.Assert.assertTrue(expected.isSameAs(resultMatrix));
+  }
+
+  @Test 
+  public void multiplicationOfTwoMatrix3X2(){
+  	Matrix a = new Matrix(2,3);
+  	Matrix b = new Matrix(3,2);
+  	a.fillMatrix(3);
+  	b.fillMatrix(4);
+  	Matrix result = a.multiply(b);
+  	// result.print();
+
+  }
+  @Test 
+  public void multiplicationOfTwoMatrix2X4(){
+  	Matrix a = new Matrix(2,4);
+  	Matrix b = new Matrix(4,2);
+  	a.fillMatrix(3);
+  	b.fillMatrix(4);
+  	Matrix result = a.multiply(b);
+  	result.print();
+
   }
 }
